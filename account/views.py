0e8d62ee83997c67a592a5faef7140fbd85c6c2e
user1 <user1@iMac-user1.local>
2018-04-28 14:28:33 +0600

    fsdfsd

--- account/views.py
+++ account/views.py
@@ -7,16 +7,10 @@ def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-            if not user_form.username and user_form.email in User.objects.all():
-            # Create a new user object but avoid saving it yet
-                new_user = user_form.save(commit=False)
-            # Set the chosen password
-                new_user.set_password(user_form.cleaned_data['password'])
-            # Save the User object
-                new_user.save()
-                return render(request, 'account/register_done.html', {'new_user': new_user})
-            else:
-                user_form = UserRegistrationForm()
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
     return render(request, 'account/register.html', {'user_form': user_form})
