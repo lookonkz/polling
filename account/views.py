5390efeb245ed7522ee4e8ba8d95cbeee7c0095e
user1 <user1@iMac-user1.local>
2018-04-28 14:02:05 +0600

    view register

--- account/views.py
+++ account/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 from .forms import LoginForm, UserRegistrationForm
+from django.contrib.auth.models import User
 
 
 def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
+            if not user_form.username and user_form.email in User.objects.all():
             # Create a new user object but avoid saving it yet
-            new_user = user_form.save(commit=False)
+                new_user = user_form.save(commit=False)
             # Set the chosen password
-            new_user.set_password(user_form.cleaned_data['password'])
+                new_user.set_password(user_form.cleaned_data['password'])
             # Save the User object
-            new_user.save()
-            return render(request, 'account/register_done.html', {'new_user': new_user})
+                new_user.save()
+                return render(request, 'account/register_done.html', {'new_user': new_user})
+            else:
+                user_form = UserRegistrationForm()
     else:
         user_form = UserRegistrationForm()
     return render(request, 'account/register.html', {'user_form': user_form})
