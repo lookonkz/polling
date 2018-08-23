0942b312da28a67e3b24f5881b50dbb4f4d0b851
user1 <user1@iMac-user1.local>
2018-08-23 15:43:10 +0600

    edit audio name

--- polling/urls.py
+++ polling/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     path('', include('musics.urls', namespace='home')),
     path('api/', include('ajax.urls', namespace='ajax')),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
