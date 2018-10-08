5b2c60d11b781ccdc125a2d34ed953a81a6df348
lookonkz <lookonkz@yandex.kz>
2018-10-08 16:17:40 +0600

    + 1

--- ajax/urls.py
+++ ajax/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from musics.views import *
 from musics.models import LikeDislike, MusicClip, MusicTrack
+from allauth.account.views import LogoutView
 
 app_name = 'ajax'
 urlpatterns = [
+    path('alogout/', LogoutView.as_view(), name='account_logout'),
     path('music/<int:pk>/like/', VotesView.as_view(model=MusicClip, vote_type=LikeDislike.LIKE),
         name='like'),
     path('music/<int:pk>/dislike/', VotesView.as_view(model=MusicClip, vote_type=LikeDislike.DISLIKE),
