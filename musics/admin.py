12330394f2e5d04b95f588021d03338498bec2bb
lookonkz <lookonkz@yandex.kz>
2018-10-18 13:57:38 +0600

    add admin

--- musics/admin.py
+++ musics/admin.py
@@ -10,15 +10,15 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
-@admin.register(LikeDislike)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("user", "object_id")
-    search_fields = ["user"]
-    list_filter = ("user", 'object_id')
-
-
-@admin.register(MusicTrack)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("name", "reiting")
-    search_fields = ["name"]
-    list_filter = ("name", "reiting")
+# @admin.register(LikeDislike)
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = ("user", "object_id", "vote")
+#     search_fields = ["user"]
+#     list_filter = ("user", "content_type", 'object_id', 'vote')
+#
+#
+# @admin.register(MusicTrack)
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = ("name", "reiting")
+#     search_fields = ["name"]
+#     list_filter = ("name", "reiting")
